3b02bdcf2487c66d5e83451b587084f088c36c12
Sri <srilalitha.ganti@gmail.com>
2021-11-05 21:49:18 +1100

    added api routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,38 @@
+const api = require("express").Router;
+const {
+  readNote,
+  readAndAppendNote,
+  readAndDeleteNote,
+} = require("../helpers/saveData");
+const { v4: uuidv4 } = require("uuid");
+
+api.get("./notes", (req, res) => {
+  readNote("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+api.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    readAndAppendNote(newNote, "./db/db.json");
+    res.json(`Added a new note`);
+  } else {
+    res.error(`Error adding note`);
+  }
+});
+
+api.delete("/notes/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("-Console Log api.del_id -" + id);
+
+  if (id) {
+    readAndDeleteNote(id, "./db/db.json");
+    res.json(`note deleted!`);
+  } else {
+    res.error(`Error deleting note`);
+  }
+});

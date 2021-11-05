80f6c03a273feef75285d626d682bc53b70561ae
Sri <srilalitha.ganti@gmail.com>
2021-11-05 19:08:39 +1100

    added code to save notes

---
+++ helpers/saveData.js
@@ -0,0 +1,39 @@
+const util = require("util");
+const fs = require("fs");
+
+const readNote = util.promisify(fs.readFile);
+const writeNote = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 2), (err) =>
+    err ? console.error(err) : console.info(`\n Data written to ${destination}`)
+  );
+
+const readAndAppendNote = (content, file) => {
+  fs.readFile(file, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeNote(file, parsedData);
+    }
+  });
+};
+const readAndDeleteNote = (id, file) => {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      filteredData = parsedData.filter((note) => id !== note.id);
+
+      // parsedData.push(content);
+      writeToFile(file, filteredData);
+    }
+  });
+};
+module.exports = {
+  readNote,
+  writeNote,
+  readAndAppendNote,
+  readAndDeleteNote,
+};

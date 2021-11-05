d0731f329539f6c5c42c4b0eb20b938b2f87b196
Sri <srilalitha.ganti@gmail.com>
2021-11-05 18:22:01 +1100

    added html routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const router = require("express").Router();
+
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/routes/index.html"));
+});
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/routes/index.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-const express = require('express');
-const apiRoutes = require('routes')
+const express = require("express");
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlroutes");
+
+const app = express();
+
+const PORT = process.env.port || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use("./api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API Server is ready on port ${PORT}!`);
+});
